ffe2e9730ac619a4c8651491fa03cad89a8aff80
Matthew Sia <matthewsia98@gmail.com>
2023-03-28 07:05:53 -0400

    remove db creds

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
+import os
+
 import psycopg2
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-DB_URL = f"postgresql+psycopg2://msia:m477h3w5@localhost:5432/ehotels"
+DB_URL = os.environ.get("EHOTELS_DATABASE_URL")
 db = SQLAlchemy()
 
 
@@ -20,17 +22,3 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
 
     return app
-
-
-def get_db_connection():
-    conn = psycopg2.connect(
-        # host="ehotelsdb.cjlormfzlvp4.ca-central-1.rds.amazonaws.com",
-        host="localhost",
-        database="ehotels",
-        # user="postgres",
-        # password="ehotelsdbpassword",
-        user="msia",
-        password="m477h3w5",
-        port="5432",
-    )
-    return conn
