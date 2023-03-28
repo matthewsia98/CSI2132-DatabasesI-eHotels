4fc3c67475ddad60da2f8c60b115ebdf1774787a
Matthew Sia <matthewsia98@gmail.com>
2023-03-28 09:32:31 -0400

    add hotels page

--- website/__init__.py
+++ website/__init__.py
@@ -2,18 +2,29 @@ import os
 
 import psycopg2
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-DB_URL = os.environ.get("EHOTELS_DATABASE_URL")
-db = SQLAlchemy()
+# from flask_sqlalchemy import SQLAlchemy
+
+# DB_URL = os.environ.get("EHOTELS_DATABASE_URL")
+# db = SQLAlchemy()
+
+EHOTELS_DB_USER = os.environ.get("EHOTELS_DB_USER")
+EHOTELS_DB_PASSWORD = os.environ.get("EHOTELS_DB_PASSWORD")
+db = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    dbname="ehotels",
+    user=EHOTELS_DB_USER,
+    password=EHOTELS_DB_PASSWORD,
+)
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "secret"
-    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 
-    db.init_app(app)
+    # app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+    # db.init_app(app)
 
     from .auth import auth
     from .views import views
--- website/views.py
+++ website/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template
 
 from . import db
-from .models import Chain
 
 views = Blueprint("views", __name__)
 
@@ -11,7 +10,47 @@ def home():
     return render_template("home.html")
 
 
-@views.route("/chains")
-def chains():
-    chains = Chain.query
+@views.route("/chains/")
+@views.route("/chains/<chain_id>")
+def chains(chain_id=None):
+    cursor = db.cursor()
+    if chain_id:
+        cursor.execute(
+            r"""SELECT *
+            FROM chains
+            WHERE chain_id = %s
+            """,
+            (chain_id),
+        )
+    else:
+        cursor.execute(
+            r"""SELECT *
+            FROM chains
+            """
+        )
+    chains = cursor.fetchall()
+    cursor.close()
     return render_template("chains.html", chains=chains)
+
+
+@views.route("/hotels/")
+@views.route("/hotels/<hotel_id>")
+def hotels(hotel_id=None):
+    cursor = db.cursor()
+    if hotel_id:
+        cursor.execute(
+            r"""SELECT *
+                FROM hotels
+                WHERE hotel_id = %s
+                """,
+            (hotel_id),
+        )
+    else:
+        cursor.execute(
+            r"""SELECT *
+                FROM hotels
+                """
+        )
+    hotels = cursor.fetchall()
+    cursor.close()
+    return render_template("hotels.html", hotels=hotels)
